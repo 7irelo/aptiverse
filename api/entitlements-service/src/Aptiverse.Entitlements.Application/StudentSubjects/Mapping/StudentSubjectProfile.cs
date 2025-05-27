47186655ee71b85f6082109547909ad5c0c300cd
7irelo <tirelo.eric@gmail.com>
2025-05-27 10:00:00 -0500

    feat(entitlements): implement application infrastructure and api

---
+++ api/entitlements-service/src/Aptiverse.Entitlements.Application/StudentSubjects/Mapping/StudentSubjectProfile.cs
@@ -0,0 +1,20 @@
+﻿using Aptiverse.Api.Application.StudentSubjects.Dtos;
+using Aptiverse.Api.Domain.Models.Students;
+using AutoMapper;
+
+namespace Aptiverse.Api.Application.StudentSubjects.Mapping
+{
+    public class StudentSubjectProfile : Profile
+    {
+        public StudentSubjectProfile()
+        {
+            CreateMap<StudentSubject, StudentSubjectDto>().ReverseMap();
+            CreateMap<StudentSubject, CreateStudentSubjectDto>().ReverseMap();
+
+            CreateMap<StudentSubject, UpdateStudentSubjectDto>()
+                .ReverseMap()
+                .ForAllMembers(opts => opts.Condition((src, dest, srcMember) =>
+                    srcMember != null && !string.IsNullOrEmpty(srcMember.ToString())));
+        }
+    }
+}
