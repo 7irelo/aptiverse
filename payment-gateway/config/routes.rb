9f89d439b22f42c3e79c35bf71d8cc26a9eb2a80
7irelo <tirelo.eric@gmail.com>
2026-02-23 23:53:36 +0200

    feat(stripe): add webhook processing endpoint

--- payment-gateway/config/routes.rb
+++ payment-gateway/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get "dashboard/index"
   post "payments/checkout_sessions", to: "payments#create_checkout_session"
+  post "webhooks/stripe", to: "webhooks/stripe#create"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
